7c9d037437f326c4c7dd40787ba3c57578964d71
willydot21 <willyusername.getall@gmail.com>
2022-08-09 23:00:18 +0200

    .

--- routes/logout.ts
+++ routes/logout.ts
@@ -1,11 +1,12 @@
 
 import express from 'express';
+import { cookieOptions } from '../models/user/user.controller';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
 
-  res.cookie('auth_token', '', { expires: new Date(0) });
+  res.cookie('auth_token', '', { expires: new Date(0), ...cookieOptions });
 
   res.end();
 
